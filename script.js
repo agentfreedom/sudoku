74b4f8cb1c5c240c5d28e4ea17fb76666e7d7f01
agentfreedom <agentfreedom@zoho.com>
2021-07-22 12:34:45 +0300

    feat: add js function to create cells with class sudoku__cell / remove basic markup in html

--- script.js
+++ script.js
@@ -2,9 +2,26 @@
 
 const game = {
 	board: '',
+	createCells() {
+		const cells = [];
+
+		for (let i = 0; i < 49; i++) {
+			const cell = document.createElement('div');
+
+			cell.className = 'sudoku__cell';
+
+			cells.push(cell);
+		}
+
+		this.cells = cells;
+
+	},
 	start(complexity) {
 		this.board = sudoku.generate(complexity);
 	}
 };
 
+game.createCells();
 game.start('medium');
+
+console.log(game);
